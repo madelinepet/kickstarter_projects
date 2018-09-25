256aebc8c416a0be414944e843e16caec2cabd0e
MADELINE PETERS <madelinepet@hotmail.com>
2018-09-24 20:28:46 -0700

    Still unclear why the db is shutting down and then reporting that the db system is ready to accept changes and then exiting with a code of 3

--- web/load_db.py
+++ web/load_db.py
@@ -1,32 +1,37 @@
-# from sqlalchemy import create_engine
-# import pandas as pd
-# import os
-# csv_data = pd.read_csv('./assets/ks-projects-201801.csv')
-# df = pd.DataFrame(csv_data)
-# # Adjust NaN values in each column, and generally clean data set
-# df['ID'] = df['Unnamed: 0'] + 1
-# # del df['Unnamed: 0']
-# df['name'] = df['name'].fillna('unknown')
-# df['category'] = df['category'].fillna('unknown')
-# df['main_category'] = df['main_category'].fillna('unknown')
-# df['currency'] = df['currency'].fillna('unknown')
-# df['deadline'] = df['deadline'].fillna('unknown')
-# df['goal'] = df['goal'].fillna('unknown')
-# df['launched'] = df['launched'].fillna('unknown')
-# df['pledged'] = df['pledged'].fillna(0.0)
-# df['state'] = df['state'].fillna('unknown')
-# df['backers'] = df['backers'].fillna('unknown')
-# df['country'] = df['country'].fillna('unknown')
-# df['usd pledged'] = df['usd pledged'].fillna('unknown')
-# df['usd_pledged_real'] = df['usd_pledged_real'].fillna('unknown')
-# df['usd_goal_real'] = df['usd_goal_real'].fillna('unknown')
+from sqlalchemy import create_engine
+import pandas as pd
+import os
 
-# db_protocol = 'postgresql'
-# db_host = os.environ.get('DB_HOST', '')
-# db_user = os.environ.get('DB_USER', '')
-# db_password = os.environ.get('DB_PASSWORD', '')
-# db_name = os.environ.get('DB_NAME', '')
-# engine = create_engine('{}://{}:{}@{}:5432/{}'.format(
-#     db_protocol, db_user, db_password, db_host, db_name
-# ))
-# df.to_sql("project_data_kickstarter", engine, if_exists='append', index=False)
+
+csv_data = pd.read_csv('./assets/ks-projects-201801.csv')
+df = pd.DataFrame(csv_data)
+
+# Adjust NaN values in each column, and generally clean data set
+df['ID'] = df['ID'].fillna(-1)
+df['name'] = df['name'].fillna('unknown')
+df['category'] = df['category'].fillna('unknown')
+df['main_category'] = df['main_category'].fillna('unknown')
+df['currency'] = df['currency'].fillna('unknown')
+df['deadline'] = df['deadline'].fillna('unknown')
+df['goal'] = df['goal'].fillna(0.0)
+df['launched'] = df['launched'].fillna('unknown')
+df['pledged'] = df['pledged'].fillna(0.0)
+df['state'] = df['state'].fillna('unknown')
+df['backers'] = df['backers'].fillna(0)
+df['country'] = df['country'].fillna('unknown')
+df['usd_pledged'] = df['usd_pledged'].fillna(0.0)
+df['usd_pledged_real'] = df['usd_pledged_real'].fillna(0.0)
+df['usd_goal_real'] = df['usd_goal_real'].fillna(0.0)
+
+db_protocol = 'postgresql'
+db_host = os.environ.get('DB_HOST', '')
+db_user = os.environ.get('DB_USER', '')
+db_password = os.environ.get('DB_PASSWORD', '')
+db_name = os.environ.get('DB_NAME', '')
+
+
+engine = create_engine('{}://{}:{}@{}:5432/{}'.format(
+    db_protocol, db_user, db_password, db_host, db_name
+))
+
+df.to_sql("project_data_project", engine, if_exists='append', index=False)
