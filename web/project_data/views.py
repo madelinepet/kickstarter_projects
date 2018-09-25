f6f3b1bd1663219d8387fdcbc1924c9be4d69455
emd5 <emahoney5@mail.greenriver.edu>
2018-09-25 12:47:03 -0700

    added caching to view.py

--- web/project_data/views.py
+++ web/project_data/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
+from django.conf import settings
 from django.core.paginator import Paginator
-
 from .models import Kickstarter
 
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+
+@cache_page(CACHE_TTL)
 def kickstarter_list_view(request):
     kickstarters_query = get_list_or_404(Kickstarter)
 
