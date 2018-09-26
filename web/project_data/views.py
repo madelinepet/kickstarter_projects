fe59f2e5d0a08a37309731bd63b3a1f078cc652c
emd5 <emahoney5@mail.greenriver.edu>
2018-09-26 13:45:19 -0700

    pagination is finally working

--- web/project_data/views.py
+++ web/project_data/views.py
@@ -18,14 +18,16 @@ def kickstarter_list_view(request):
     page = request.GET.get('page')
     kickstarters = paginator.get_page(page)
 
+    # import pdb; pdb.set_trace()
+
     context = {
         'kickstarters': kickstarters
     }
-    return render(request, 'reviews/kickstarter_list.html', context=context)
+    return render(request, 'reviews/kickstarter_list.html', context)
 
 
 def kickstarter_detail_view(request, pk=None):
     context = {
         'kickstarter': get_object_or_404(Kickstarter, id=pk)
     }
-    return render(request, 'reviews/kickstarter_detail.html', context=context)
+    return render(request, 'reviews/kickstarter_detail.html', context)
